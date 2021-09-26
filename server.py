2e7ad74f361e3190116623b8e78118b5a3a4586e
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-09-26 19:53:46 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from app import routes, app
+from flask import Flask
+app = Flask(__name__)
 
-# ------------------------------
- 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8080,threaded=True)
+    app.run(host='0.0.0.0',ssl_context=('cert.pem', 'key.pem'), port=443)
